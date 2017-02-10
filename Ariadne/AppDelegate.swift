2c516a91c5279937cd38f3da6c6e9cac0d7a435d
Korriban <kling.mobil@gmail.com>
2017-02-10 21:19:39 +0100

    Initial Commit

---
+++ Ariadne/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Ariadne
+//
+//  Created by Christian Kling on 10/02/2017.
+//  Copyright © 2017 Fennheim. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
