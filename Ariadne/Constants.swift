214846f1af2abf84437008bc775f5a27202501fb
Korriban <kling.mobil@gmail.com>
2017-02-20 23:22:28 +0100

    adding Constants

---
+++ Ariadne/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Ariadne
+//
+//  Created by Christian Kling on 20/02/2017.
+//  Copyright © 2017 Fennheim. All rights reserved.
+//
+
+let PlayerCategory   : UInt32 = 0x1 << 1
+let EnemyCategory    : UInt32 = 0x1 << 2
+let ArrowCategory    : UInt32 = 0x1 << 3
+let ParticleCategory : UInt32 = 0x1 << 4
+let WorldCategory    : UInt32 = 0x1 << 5
